85e7b71b8f173a219b45164e8e042e2a0baf2a46
mattak <mattak.me@gmail.com>
2015-01-31 18:20:59 +0900

    ADD: MoneyController

---
+++ Assets/Script/MoneyController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.rigidbody2D.velocity = new Vector2(0, this.gameObject.rigidbody2D.velocity.y);
+	}
+}
