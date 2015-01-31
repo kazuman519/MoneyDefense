9677f546a75bf831e14c777d0003b46f397bf6c1
mattak <mattak.me@gmail.com>
2015-01-31 16:07:06 +0900

    FIX: Oyagi Touch Event is wrong.

--- Assets/Script/OyagiController.cs
+++ Assets/Script/OyagiController.cs
@@ -18,10 +18,12 @@ public class OyagiController: MonoBehaviour {
 			move (speed);
 		}
 
-		if (Input.touchCount > 0 && Input.GetTouch(0).position.x > Screen.width / 2) {
-			move(speed);
-		} else {
-			move(-speed);
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch(0).position.x > Screen.width / 2) {
+				move(speed);
+			} else {
+				move(-speed);
+			}
 		}
 	}
 
