87403dd8fbccb259a1204b7ea87aff31be58c8b5
mattak <mattak.me@gmail.com>
2015-02-01 13:30:50 +0900

    ADD: {Wallet,ATM}Controller added

---
+++ Assets/Script/WalletController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WalletController : MonoBehaviour {
+	public Text text; // The object which desplay the balance of balance.
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
