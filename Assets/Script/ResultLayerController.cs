f32fb6cae7b38112bd1d64cf900b52c34559fe43
mattak <mattak.me@gmail.com>
2015-02-01 16:24:53 +0900

    ADD: GameObserver, ResultLayerController

---
+++ Assets/Script/ResultLayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResultLayerController : MonoBehaviour {
+	public Text resultCashText;
+
+	// Use this for initialization
+	void Start () {
+		SetText (0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void SetText(float resultCash) {
+		resultCashText.text = resultCash.ToString();
+	}
+
+	public void ShowResultLayer(float resultCash) {
+		this.gameObject.SetActive(true);
+		SetText (resultCash);
+	}
+
+	public void HideResultLayer() {
+		this.gameObject.SetActive(false);
+	}
+}
