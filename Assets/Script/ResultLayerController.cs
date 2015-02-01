fa1e4657e6d800563f8e72d541fd5d6d011c19d6
mattak <mattak.me@gmail.com>
2015-02-01 17:29:08 +0900

    ADD: ResultLayer play sounds

--- Assets/Script/ResultLayerController.cs
+++ Assets/Script/ResultLayerController.cs
@@ -4,10 +4,16 @@ using System.Collections;
 
 public class ResultLayerController : MonoBehaviour {
 	public Text resultCashText;
+	public AudioClip audioClipResultFail;
+	public AudioClip audioClipResultOK;
+	public AudioClip audioClipResultGood;
+	public AudioClip audioClipResultGreat;
+	public AudioClip audioClipResultPerfect;
 
 	// Use this for initialization
 	void Start () {
 		SetText (0);
+
 	}
 	
 	// Update is called once per frame
@@ -18,9 +24,30 @@ public class ResultLayerController : MonoBehaviour {
 		resultCashText.text = resultCash.ToString();
 	}
 
-	public void ShowResultLayer(float resultCash) {
+	void PlayAudioClip(AudioClip clip) {
+		audio.clip = clip;
+		audio.Play ();
+	}
+
+	AudioClip SelectAudioClip(float resultCash, float startCash) {
+		float percentage = resultCash / startCash;
+		if (percentage >= 100.0f) {
+			return audioClipResultPerfect;
+		} else if (percentage >= 90.0f) {
+			return audioClipResultGreat;
+		} else if (percentage >= 80.0f) {
+			return audioClipResultGood;
+		} else if (percentage <= 0.0f) {
+			return audioClipResultFail;
+		} else {
+			return audioClipResultOK;
+		}
+	}
+
+	public void ShowResultLayer(float resultCash, float startCash) {
 		this.gameObject.SetActive(true);
 		SetText (resultCash);
+		PlayAudioClip( SelectAudioClip (resultCash, startCash) );
 	}
 
 	public void HideResultLayer() {
