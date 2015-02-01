c11290364e50212e93fe09377e9a71a039aea2d4
mattak <mattak.me@gmail.com>
2015-02-01 15:15:55 +0900

    ADD: JK wait & dispapper logic added

--- Assets/Script/JKController.cs
+++ Assets/Script/JKController.cs
@@ -4,20 +4,67 @@ using System.Collections;
 public class JKController: MonoBehaviour {
 	public float speed = 1;
 	public string tagDisappear = "Money10k";
+	public string[] tagsWait = {"Money1k", "Money5k"};
+	private float stopWaitTime = 10f;
+	private float previousStopTime;
+	private bool isRequestStop;
 
 	// Use this for initialization
 	void Start () {
+		isRequestStop = false;
+		previousStopTime = Time.realtimeSinceStartup;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 velocity = this.gameObject.rigidbody2D.velocity;
-		this.gameObject.rigidbody2D.velocity = new Vector2(-speed, velocity.y);
+		if (ShouldStop()) {
+			Stop ();
+		} else {
+			Run ();
+		}
 	}
 	
 	void OnTriggerEnter2D (Collider2D c) {
 		if (c.tag == tagDisappear) {
 			Object.Destroy (this.gameObject);
+		} else if (isTagWait(c.tag)) {
+			RequestStop(10.0f);
 		}
+	}
+
+	bool isTagWait(string tag) {
+		foreach (string canonicalTag in tagsWait) {
+			if (tag == canonicalTag) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void RequestStop(float waitTime) {
+		stopWaitTime = waitTime;
+		isRequestStop = true;
+	}
+
+	bool ShouldStop() {
+		float time = Time.realtimeSinceStartup;
+
+		if (time >= previousStopTime + stopWaitTime ) {
+			previousStopTime = time;
+			isRequestStop = false;
+			return true;
+		}
+
+		return false;
+	}
+
+	void Run () {
+		Vector2 velocity = this.gameObject.rigidbody2D.velocity;
+		this.gameObject.rigidbody2D.velocity = new Vector2(-speed, velocity.y);
+	}
+
+	void Stop () {
+		Vector2 velocity = this.gameObject.rigidbody2D.velocity;
+		this.gameObject.rigidbody2D.velocity = new Vector2(0, velocity.y);
 	}
 }
