1e46dc637c0789de09df1877d5e132de7ca2cef7
mattak <mattak.me@gmail.com>
2015-02-01 14:57:29 +0900

    FIX: JK disappear when touch 10k money.

--- Assets/Script/JKController.cs
+++ Assets/Script/JKController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class JKController: MonoBehaviour {
 	public float speed = 1;
+	public string tagDisappear = "Money10k";
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +13,11 @@ public class JKController: MonoBehaviour {
 	void Update () {
 		Vector2 velocity = this.gameObject.rigidbody2D.velocity;
 		this.gameObject.rigidbody2D.velocity = new Vector2(-speed, velocity.y);
+	}
+	
+	void OnTriggerEnter2D (Collider2D c) {
+		if (c.tag == tagDisappear) {
+			Object.Destroy (this.gameObject);
+		}
 	}
 }
